db7466bcb5e8f1c3529ad85ff67ee76d1d23096f
unknown <jameslester78@gmail.com>
2023-11-29 22:57:32 +0000

    more

--- src/js/data/2023-11-29.js
+++ src/js/data/2023-11-29.js
@@ -9,7 +9,8 @@ dataSet[dataSetVersion].options = [
     checked: false,
     sub: [
       { name: "Generation Terrorists", key: "GT" },
-      { name: "Gold Against The Soul", key: "GATS" }
+      { name: "Gold Against The Soul", key: "Gold" },
+      { name: "The Holy Bible", key: "THB" }
     ]
   }
 ];
@@ -44,10 +45,255 @@ dataSet[dataSetVersion].characterData = [
     }
   },
   {
+    name: "You Love Us",
+    img: "NcHbmjf.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "Love's Sweet Exile",
+    img: "NcHbmjf.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "Little Baby Nothing",
+    img: "NcHbmjf.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "Repeat",
+    img: "NcHbmjf.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "Tennessee",
+    img: "NcHbmjf.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "Another Invented Disease",
+    img: "NcHbmjf.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "Stay Beautiful",
+    img: "NcHbmjf.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "So Dead",
+    img: "NcHbmjf.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "Spectators of Suicide",
+    img: "NcHbmjf.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "Damn Dog",
+    img: "NcHbmjf.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "Crucifix Kiss",
+    img: "NcHbmjf.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "Methadone Pretty",
+    img: "NcHbmjf.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "Condemned to Rock 'n' Roll",
+    img: "NcHbmjf.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
     name: "Sleepflower",
     img: "YrVV6Y6.jpg",
     opts: {
       series: [ "Gold"]
+    }
+  },
+  {
+    name: "From Despair to Where",
+    img: "YrVV6Y6.jpg",
+    opts: {
+      series: [ "Gold"]
+    }
+  },
+  {
+    name: "La Tristesse Durera (Scream to a Sigh)",
+    img: "YrVV6Y6.jpg",
+    opts: {
+      series: [ "Gold"]
+    }
+  },
+  {
+    name: "Yourself",
+    img: "YrVV6Y6.jpg",
+    opts: {
+      series: [ "Gold"]
+    }
+  },
+  {
+    name: "Life Becoming a Landslide",
+    img: "YrVV6Y6.jpg",
+    opts: {
+      series: [ "Gold"]
+    }
+  },
+  {
+    name: "Drug Drug Druggy",
+    img: "YrVV6Y6.jpg",
+    opts: {
+      series: [ "Gold"]
+    }
+  },
+  {
+    name: "Roses in the Hospital",
+    img: "YrVV6Y6.jpg",
+    opts: {
+      series: [ "Gold"]
+    }
+  },
+  {
+    name: "Nostalgic Pushead",
+    img: "YrVV6Y6.jpg",
+    opts: {
+      series: [ "Gold"]
+    }
+  },
+  {
+    name: "Symphony of Tourette",
+    img: "YrVV6Y6.jpg",
+    opts: {
+      series: [ "Gold"]
+    }
+  },
+  {
+    name: "Gold Against the Soul",
+    img: "YrVV6Y6.jpg",
+    opts: {
+      series: [ "Gold"]
+    }
+  },
+  {
+    name: "Yes",
+    img: "9uyyXwf.jpg",
+    opts: {
+      series: [ "THB"]
+    }
+  },
+  {
+    name: "Ifwhiteamericatoldthetruthforonedayit'sworldwouldfallapart",
+    img: "9uyyXwf.jpg",
+    opts: {
+      series: [ "THB"]
+    }
+  },
+  {
+    name: "Of Walking Abortion",
+    img: "9uyyXwf.jpg",
+    opts: {
+      series: [ "THB"]
+    }
+  },
+  {
+    name: 	"She Is Suffering",
+    img: "9uyyXwf.jpg",
+    opts: {
+      series: [ "THB"]
+    }
+  },
+  {
+    name: "Archives of Pain",
+    img: "9uyyXwf.jpg",
+    opts: {
+      series: [ "THB"]
+    }
+  },
+  {
+    name: "Revol",
+    img: "9uyyXwf.jpg",
+    opts: {
+      series: [ "THB"]
+    }
+  },
+  {
+    name: "4st 7lb",
+    img: "9uyyXwf.jpg",
+    opts: {
+      series: [ "THB"]
+    }
+  },
+  {
+    name: "Mausoleum",
+    img: "9uyyXwf.jpg",
+    opts: {
+      series: [ "THB"]
+    }
+  },
+  {
+    name: "Faster",
+    img: "9uyyXwf.jpg",
+    opts: {
+      series: [ "THB"]
+    }
+  },
+  {
+    name: "This Is Yesterday",
+    img: "9uyyXwf.jpg",
+    opts: {
+      series: [ "THB"]
+    }
+  },
+  {
+    name: "Die in the Summertime",
+    img: "9uyyXwf.jpg",
+    opts: {
+      series: [ "THB"]
+    }
+  },
+  {
+    name: "The Intense Humming of Evil",
+    img: "9uyyXwf.jpg",
+    opts: {
+      series: [ "THB"]
+    }
+  },
+  {
+    name: "P.C.P.",
+    img: "9uyyXwf.jpg",
+    opts: {
+      series: [ "THB"]
     }
   }
 ];
