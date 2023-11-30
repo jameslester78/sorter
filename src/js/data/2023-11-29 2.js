a4a0299877ebd9ca22106fb07bf3b12ea9bd1f1c
unknown <jameslester78@gmail.com>
2023-11-30 09:26:01 +0000

    more

---
+++ src/js/data/2023-11-29 2.js
@@ -0,0 +1,65 @@
+dataSetVersion = "2021-05-10"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Leave This Unchecked",
+    key: "series",
+    checked: false,
+    sub: [
+      { name: "", key: "GT" }
+    ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Generation Terrorists",
+    img: "NcHbmjf.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "Gold Against The Soul",
+    img: "YrVV6Y6.jpg",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "The Holy Bible",
+    img: "yt0RqAY.png",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "Everything Must Go",
+    img: "wlnUv33.png",
+    opts: {
+      series: [ "GT"]
+    }
+  },
+  {
+    name: "This is My Truth",
+    img: "Enwozds.png",
+    opts: {
+      series: [ "Gold"]
+    }
+  },
+  {
+    name: "Know Your Enemy",
+    img: "hBhoPwW.png",
+    opts: {
+      series: [ "Gold"]
+    }
+  },
+  {
+    name: "Lifeblood",
+    img: "QDeRaiz.png",
+    opts: {
+      series: [ "Gold"]
+    }
+  }
+];
